7c9ab703e889fd5867bab36125bad6f709db480f
Ira <ira@macs-MacBook-Air.local>
2020-01-07 12:41:28 +1100

    test1 app working

---
+++ test1/Readme.md
@@ -0,0 +1,5 @@
+# Simple Go API endpoint using HTTProuter to provide API routing
+
+[HTTProuter](https://github.com/julienschmidt/httprouter):
+ - Low Latency http router with [better performance](https://github.com/julienschmidt/go-http-routing-benchmark) than the default http.ServeMux and other HTTP/API router frameworks
+
---
+++ test1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"os"
+    "log"
+    "github.com/julienschmidt/httprouter"
+)
+
+// Need to have a nested struct e.g. Data{App{}}, as JSON NewEncoder.Encode returns nothing when calling an unnested struct. 
+
+type Data struct {
+	App App `json:"myapplication"`
+}
+
+type App struct {
+	Version string `json:"version"`
+	Lastcommitsha string `json:"lastcommitsha"`
+	Description string `json:"description"`
+}
+
+func Version(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	myapp := new(App)
+	myapp.Version = os.Getenv("VERSION")
+	myapp.Lastcommitsha = os.Getenv("LASTCOMMITSHA")
+	myapp.Description = "pre-interview technical test"
+
+	p := Data{App: *myapp} 
+	json.NewEncoder(w).Encode(p)
+
+}
+
+func main() {
+    router := httprouter.New()
+    router.GET("/api/version", Version)
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
