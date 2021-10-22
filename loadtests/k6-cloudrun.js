af9438b4053b2d48ba7600be924b51b5d70f787c
Ira <ira@ira.nz>
2021-10-22 17:10:06 +1300

    K6 working locally

--- loadtests/k6-cloudrun.js
+++ loadtests/k6-cloudrun.js
@@ -2,22 +2,22 @@
 
 import "./libs/shim/core.js";
 
-export let options = { maxRedirects: 4, iterations: "10000" };
+export let options = { maxRedirects: 2, iterations: "10000" };
 
 const Request = Symbol.for("request");
 postman[Symbol.for("initial")]({
   options
 });
 
-let file_path = '/workspace/cloudrun_address'
-const URL = open(file_path)
+let file_path = './cloudrun_address'
+const URL = open(file_path) + "/version"
 
 export default function() {
   postman[Request]({
     name: "devops-lab1",
     id: "015cb1b5-c0da-4c8f-9845-29952dda213b",
     method: "GET",
-    address: "${URL}/version",
+    address: URL,
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     }
