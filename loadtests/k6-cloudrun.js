d1c0499cf2a12f437572d80274fcc5f278b8567c
Ira <ira@ira.nz>
2021-10-22 19:17:26 +1300

    hmm

--- loadtests/k6-cloudrun.js
+++ loadtests/k6-cloudrun.js
@@ -4,7 +4,12 @@ import "./libs/shim/core.js";
 
 export let options = {
   maxRedirects: 2,
-  iterations: "100000",
+  stages: [
+    { duration: '1m', target: 200 },
+    { duration: '1m', target: 500 },
+    { duration: '1m', target: 1000 },
+    { duration: '1m', target: 2000 },
+  ],
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
